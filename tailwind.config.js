3a24d33e128feaebad19b485d9c6cd44eb35c92b
Toan Lam <toanlam01@gmail.com>
2021-02-24 18:23:16 -0800

    first branch to deploy ever

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ["src/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+    },
+
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
